6e9d0012e1fc6f530fb89b6c2c46890a66267b4a
julien <julien.tallagrand@gmail.com>
2022-05-11 13:49:33 +0200

    US8 : Creation service modifier Event + creation conponent EventEdit

--- src/pages/Events/index.js
+++ src/pages/Events/index.js
@@ -1,7 +1,8 @@
-import "./EventsList.css";
+import "./Events.css";
 import { useState, useEffect } from "react";
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import Eventslist from "../../components/EventsList";
+import EventsAdd from "../../components/EventsAdd";
 
 const Events = () => {
   const [events, setEvents] = useState([]);
@@ -10,7 +11,13 @@ const Events = () => {
     <Container>
       <Row className="justify-content-center">
         <Col sm>
-          <p class="h5 m-4">Liste des évènnement</p>
+          <p className="h5 m-4">Ajouter un évènnement</p>
+          <EventsAdd isCreate={true} />
+        </Col>
+      </Row>
+      <Row className="justify-content-center">
+        <Col sm>
+          <p className="h5 m-4">Liste des évènnement</p>
           <Eventslist events={events} setEvents={setEvents} />
         </Col>
       </Row>
