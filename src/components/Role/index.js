a34be5fb00c1d91e266cf5d851fcf36a06bdcc31
Christian ly <christian_ly@outlook.fr>
2022-05-12 16:19:17 +0200

    Ajout vérification avant de supprimer un role

--- src/components/Role/index.js
+++ src/components/Role/index.js
@@ -1,8 +1,7 @@
 import React from "react";
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Badge, Button, Card, Col } from "react-bootstrap";
-import { useNavigate, history } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import services from "../../services";
 import("./Role.css");
 
@@ -12,10 +11,21 @@ export default function Role(role) {
   const deleteRole = (idRole) => {
     console.log("==> deleteRole", idRole);
     services
-      .deleteRole(idRole)
-      .then((response) => {
-        console.log(response);
-        navigate(0);
+      .countParticipantsByRole(idRole)
+      .then((count) => {
+        console.log(count);
+        //On supprime aucun participant utilise le role
+        if(count === 0) {
+          services
+          .deleteRole(idRole)
+          .then((response) => {
+            console.log(response);
+            navigate(0);
+          })
+          .catch(console.log);
+        } else {
+          alert("Role utilisé par les participants, vous ne pouvez pas le supprimer");
+        }
       })
       .catch(console.log);
   };
